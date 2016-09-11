a9634af65be4c0fbd524784bcb756bb8396319f6
kpayak <payak.keyur@gmail.com>
2016-09-11 12:09:12 -0700

    Added filtering in list functionality

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var placesService;
 var viewmodel = {};
 var markers = [];
 var bounds;
+var infowindow;
+
 
 //Marker colors:
 var markerColorHome = '19B5FE';
@@ -91,8 +93,34 @@ function addMarker(position, title, markerText, markerColor, id) {
     //Dont push "Home" Marker into the markers array.
     if (id !== "home") {
         markers.push(marker);
+        marker.addListener('click', function () {
+            populateInfoWindow(this);
+        });
     }
+}
 
+function findMarkerIndex(id) {
+    var i;
+    for (i = 0; i < markers.length; i++) {
+        if (markers[i].id === id) {
+            return i;
+            break;
+        }
+    }
+}
+
+function populateInfoWindow(marker) {
+    var index = findMarkerIndex(marker.id);
+    console.log(viewModel.listOfPlaces()[index].name);
+    var nameString = "<div class='infowindow-header'><div><h2>" + viewModel.listOfPlaces()[index].name + "</div></h2>";
+    var iconString = "<div><img class='infowindow-img' src=" + viewModel.listOfPlaces()[index].icon + "></div></div>";
+    var addressString = "<p class='infowindow-text'>" + viewModel.listOfPlaces()[index].address + "</p>";
+    var phoneString = "<p class='infowindow-text'>" + viewModel.listOfPlaces()[index].phone + " | ";
+    var websiteString = "<a href=" + viewModel.listOfPlaces()[index].website + ">Website</a>" + "</p>";
+    var hoursString = "<p class='infowindow-text'>Current Status: " + viewModel.listOfPlaces()[index].hours + "</p>";
+    contentString = iconString + nameString + addressString + phoneString + websiteString + hoursString;
+    infowindow.setContent(contentString);
+    infowindow.open(map, marker);
 }
 
 function clearMarkers() {
@@ -114,7 +142,14 @@ function updatePlaceObject(p) {
 
     var place = {};
 
+    //Flag to show/hide in "view". Default set to true
+    place.show = ko.observable(true);
+
+    //Place id
     place.id = p.place_id;
+
+    //Place icon
+    place.icon = p.icon;
 
     if (p.name) {
         place.name = p.name;
@@ -153,6 +188,25 @@ function updatePlaceObject(p) {
         };
     }
 
+    //Add website info
+    if (p.website) {
+        place.website = p.website;
+    } else {
+        place.website = "No Website found for this place."
+    }
+
+    //Add Open Hours info
+    if (p.opening_hours) {
+        if (p.opening_hours.open_now) {
+            place.hours = "Open Now";
+        } else {
+            place.hours = "Not available";
+        }
+
+    } else {
+        place.hours = "Closed for today";
+    }
+
     //Add marker for each place
     viewModel.listOfPlaces.push(place);
     addMarker({
@@ -177,8 +231,8 @@ function googleNearbySearch(position) {
     var currentLatLng = map.getCenter();
     var initialRequest = {
         location: position,
-        radius: 6000,
-        types: ['restaurant', 'cafe', 'bus_station', 'city_hall', 'department_store', 'home_goods_store', 'transit_station', 'train_station', 'subway_station', 'shopping_mall', 'museum', 'library', 'hospital']
+        radius: 10000,
+        types: ['restaurant', 'cafe', 'bus_station', 'city_hall', 'department_store', 'home_goods_store', 'transit_station', 'train_station', 'subway_station', 'shopping_mall', 'museum', 'library']
     };
     placesService.nearbySearch(initialRequest, callback);
 }
@@ -186,25 +240,41 @@ function googleNearbySearch(position) {
 function callback(results, status) {
     //Clear list of places before updating it with search results
     viewModel.listOfPlaces.removeAll();
-
-    //Clear all markers before updating results
-
-    for (var i = 0; i < results.length; i++) {
-        var place = placesService.getDetails({
-            placeId: results[i].place_id
-        }, function (p, status) {
-            if (status === "OK") {
-                updatePlaceObject(p);
-            }
-        });
+    if (status === "OK") {
+        //Clear any lingering html
+        $('.locations-list').html("");
+        for (var i = 0; i < results.length; i++) {
+            var place = placesService.getDetails({
+                placeId: results[i].place_id
+            }, function (p, status) {
+                if (status === "OK") {
+                    updatePlaceObject(p);
+                }
+            });
+        }
+    } else {
+        console.log("no results");
+        $('.locations-list').html("No results match your query.");
     }
+
 }
 
 function startSearch() {
     //Listen to click events of search icon
-    var searchQuery;
-    searchQuery = $('input').val();
-    googleTextSearch(map.getCenter(), searchQuery);
+
+    var searchQuery = $('input').val().toLocaleLowerCase();
+    var lowerCaseName = "";
+    console.log("start search: " + searchQuery);
+    for (var i = 0; i < viewModel.listOfPlaces().length; i++) {
+        lowerCaseName = viewModel.listOfPlaces()[i].name.toLowerCase();
+        if (lowerCaseName.search(searchQuery) !== -1) {
+            viewModel.listOfPlaces()[i].show(true);
+        } else {
+            viewModel.listOfPlaces()[i].show(false);
+        }
+    }
+
+    //googleTextSearch(map.getCenter(), searchQuery);
     reCenterBounds();
 }
 
@@ -216,23 +286,21 @@ function initMap() {
         zoom: 13
     });
 
+    //Infowindow 
+    infowindow = new google.maps.InfoWindow();
+
+    //Remove infowindow if map is clicked
+    google.maps.event.addListener(map, 'click', function () {
+        infowindow.close();
+    });
+
     /* Re-center map based on user's current location */
     setCurrentLocation();
     reCenterBounds();
-    $('.search-icon').click(startSearch);
-    console.log(viewModel.listOfPlaces());
-    console.log(markers);
+    $('.search').keyup(startSearch);
 }
 
-function findMarkerIndex(id) {
-    var i;
-    for (i = 0; i < markers.length; i++) {
-        if (markers[i].id === id) {
-            return i;
-            break;
-        }
-    }
-}
+
 
 /* KO Starts Here */
 
@@ -240,7 +308,6 @@ viewModel = {
     listOfPlaces: ko.observableArray(),
     highlightMarker: function () {
         var markerIndex = findMarkerIndex(this.id);
-        console.log("markerIndex: " + markerIndex);
         var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + (markerIndex + 1) + "|" + markerColorHover + "|FFF");
         markers[markerIndex].setIcon(pinImage);
     },
